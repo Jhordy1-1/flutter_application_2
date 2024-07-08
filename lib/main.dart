8bebad39d31925fdca762543555adc52953a281a
Jhordy11 <fjhordy1@gmail.com>
2024-07-08 12:30:14 -0500

    avance xd

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,41 @@ class MyApp extends StatelessWidget {
         body: const SingleChildScrollView(
             child: Column(
           children: [
-            TitleSection(name: "Panecillo", location: "Quito/Ecuador"),
-            TitleSection(name: "La mitad del mundo", location: "Quito/Ecuador"),
-            TitleSection(name: "Laguna del Quilotoa", location: "Latacunga/Ecuador"),
-            TitleSection(name: "Baños", location: "Baños/Ecuador"),
-            TitleSection(name: "La ronda", location: "Quito/Ecuador"),
-            ButtonSection(),
-            TextSection(description: "Descripcion de la ronda"),
-            ImageSection(image: "images/lake.jpeg")
+            TitleSection(
+              name: "Galápagos",
+              location: "Galápagos/Ecuador",
+              description:
+                  "El archipiélago de Galápagos es conocido mundialmente por su increíble biodiversidad y su influencia en la teoría de la evolución de Charles Darwin. Las islas ofrecen oportunidades únicas para observar especies animales que no se encuentran en ningún otro lugar del mundo, como las tortugas gigantes y las iguanas marinas.",
+              image: "images/galapagos.jpg",
+            ),
+            TitleSection(
+              name: "La mitad del mundo",
+              location: "Quito/Ecuador",
+              description:
+                  "La Mitad del Mundo es un monumento ubicado en la parroquia de San Antonio de Pichincha, al norte de Quito. Marca el lugar donde se supone que pasa la línea ecuatorial. Es un sitio turístico popular que ofrece museos y actividades relacionadas con la ciencia y la cultura.",
+              image: "images/mitad.jpeg",
+            ),
+            TitleSection(
+              name: "Laguna del Quilotoa",
+              location: "Latacunga/Ecuador",
+              description:
+                  "La Laguna del Quilotoa es un cráter volcánico lleno de agua turquesa, ubicado en la provincia de Cotopaxi. Es un destino popular para el senderismo y ofrece vistas impresionantes del cráter y el paisaje circundante.",
+              image: "images/quilotoa.jpg",
+            ),
+            TitleSection(
+              name: "Baños",
+              location: "Baños/Ecuador",
+              description:
+                  "Conocido simplemente como Baños, este pequeño pueblo es famoso por sus cascadas y aguas termales. Es un destino popular para los amantes de la aventura, ofreciendo actividades como senderismo, ciclismo, rafting y puenting. Además, el paisaje montañoso circundante es espectacular.",
+              image: "images/banios.jpg",
+            ),
+            TitleSection(
+              name: "La ronda",
+              location: "Quito/Ecuador",
+              description:
+                  "La Ronda es una calle emblemática en el centro histórico de Quito. Conocida por su encanto colonial, ofrece una variedad de tiendas, restaurantes y bares. Es un lugar ideal para experimentar la cultura local y disfrutar de la vida nocturna quiteña.",
+              image: "images/ronda.jpg",
+            ),
           ],
         )),
       ),
@@ -33,48 +60,58 @@ class MyApp extends StatelessWidget {
 }
 
 class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
+  const TitleSection(
+      {super.key,
+      required this.name,
+      required this.location,
+      required this.description,
+      required this.image});
 
   final String name;
   final String location;
+  final String description;
+  final String image;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32),
-      child: Row(
+      child: Column(
         children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        name,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
+                    Text(
+                      location,
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.red[500],
+              ),
+              const Text('41'),
+            ],
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          ButtonSection(),
+          TextSection(description: description),
+          ImageSection(image: image),
         ],
       ),
     );
@@ -146,6 +183,7 @@ class ButtonWithText extends StatelessWidget {
     );
   }
 }
+
 class TextSection extends StatelessWidget {
   const TextSection({
     super.key,
@@ -165,6 +203,7 @@ class TextSection extends StatelessWidget {
     );
   }
 }
+
 class ImageSection extends StatelessWidget {
   const ImageSection({super.key, required this.image});
 
